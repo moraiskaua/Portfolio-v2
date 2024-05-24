6075ae40b6f60986878c184b6411534d0fb445d2
moraiskaua <moraiskaua03@gmail.com>
2024-05-24 16:05:42 -0300

    create grid component

---
+++ src/app/components/Grid.tsx
@@ -0,0 +1,39 @@
+import { gridItems } from '@/data';
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid';
+
+interface GridProps {}
+
+const Grid: React.FC<GridProps> = ({}) => {
+  return (
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map(
+          ({
+            id,
+            title,
+            description,
+            className,
+            img,
+            imgClassName,
+            spareImg,
+            titleClassName,
+          }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              spareImg={spareImg}
+              titleClassName={titleClassName}
+            />
+          ),
+        )}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import { navItems } from '@/data';
 import Hero from './components/Hero';
+import { FloatingNavbar } from './components/ui/FloatingNavbar';
+import Grid from './components/Grid';
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNavbar navItems={navItems} />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
