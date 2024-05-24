c30a4b154b14bbb7b73ed6af276eca2bc7d32c33
moraiskaua <moraiskaua03@gmail.com>
2024-05-24 15:11:20 -0300

    fix: change text generate component props

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,4 +1,5 @@
 import { Spotlight } from './ui/Spotlight';
+import { TextGenerateEffect } from './ui/TextGenerateEffect';
 
 interface HeroProps {}
 
@@ -26,6 +27,15 @@ const Hero: React.FC<HeroProps> = ({}) => {
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web Magic with Next.js
           </h2>
+
+          <TextGenerateEffect
+            words="Transforming Concepts into Seamless Experiences"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I&apos;m Kauã, a Full-stack Developer.
+          </p>
         </div>
       </div>
     </div>
