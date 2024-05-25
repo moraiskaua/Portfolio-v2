00e9190510ff02defe9dbf98674c7ee1b24c70a4
moraiskaua <moraiskaua03@gmail.com>
2024-05-25 00:52:30 -0300

    fix: overflow content

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,14 +8,6 @@ interface FooterProps {}
 const Footer: React.FC<FooterProps> = ({}) => {
   return (
     <footer className="w-full pt-20" id="contact">
-      <div className="w-full absolute left-0 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50"
-        />
-      </div>
-
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw] mb-2 lg:m-0">
           Ready to take <span className="text-purple">your</span> digital
