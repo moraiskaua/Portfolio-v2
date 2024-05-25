739c5692ed056c89dc12fc1f7bd1d7ac830cfdd2
moraiskaua <moraiskaua03@gmail.com>
2024-05-25 00:44:08 -0300

    fix: text color of project description

--- src/app/components/RecentProjects.tsx
+++ src/app/components/RecentProjects.tsx
@@ -47,7 +47,13 @@ const RecentProjects: React.FC<RecentProjectsProps> = ({}) => {
                   {name}
                 </h1>
 
-                <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
+                <p
+                  className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+                  style={{
+                    color: '#BEC1DD',
+                    margin: '1vh 0',
+                  }}
+                >
                   {description}
                 </p>
 
